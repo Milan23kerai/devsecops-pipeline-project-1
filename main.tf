fba7301ed581646dcd3f453ffc936aa41ca519b9
Milan <milan23kerai@gmail.com>
2026-03-24 15:13:55 +0000

    update 3

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraformbuckettodaysunmoon" {
+  bucket = "terraformbuckettodaysunmoon"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformbuckettodaysunmoon"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
